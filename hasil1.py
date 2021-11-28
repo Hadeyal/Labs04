c270fe9bdbd3704a153770fc17fe5655bbf2a4d8
Hadeyal <hadeyalgans@gmail.com>
2021-11-28 15:28:10 +0700

    Menambahkan File

---
+++ hasil1.py
@@ -0,0 +1,15 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python38-32/list.py ===
+Membuat list: [2001, 2002, 2003, 2004, 2005]
+Menampilkan element 3:  2003
+Mengambil element ke 2 sampai 4:  [2002, 2003, 2004]
+Mengambil element terakhir:  2005
+Mengubah element ke 4 dengan niai lain: [2001, 2002, 2003, 2006, 2005]
+Mengubah element ke 4 sampai element terakhir: [2001, 2002, 2003, 2006, 2007]
+Mengambil 2 bagian dari list pertama: [2004, 2005]
+Membuat list kedua : [2004, 2005]
+Menambah list kedua dengan 3 nilai: [2004, 2005, 2006, 2007, 2008]
+Menggabungkan list 1 dan list 2 : [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008]
+>>> 
